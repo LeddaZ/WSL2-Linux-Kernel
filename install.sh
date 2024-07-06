52b09b90e8b18268cfc4b8e0993b0cb71949e9bc
Leonardo Ledda <leonardoledda@gmail.com>
2024-07-07 01:13:27 +0200

    Add info about removing old modules in install script
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- install.sh
+++ install.sh
@@ -15,3 +15,4 @@ cp -fr lib /
 cp -fr include /usr
 echo -e "${GREEN}Installing modules and headers finished.${DEFAULT}"
 echo -e "${CYAN}Script finished. To install the kernel, set the path as shown here: https://learn.microsoft.com/en-us/windows/wsl/wsl-config#example-wslconfig-file${DEFAULT}"
+echo -e "${CYAN}After loading the new kernel, you can delete the modules for the previous one from /lib/modules.${DEFAULT}"
