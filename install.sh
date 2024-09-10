dfc9337ad950a4f5909793aac2943ca991106e9d
Leonardo Ledda <leonardoledda@gmail.com>
2024-09-10 03:37:44 +0200

    Don't install headers by default
    * Run install.sh with the -h flag to install headers.
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- install.sh
+++ install.sh
@@ -9,10 +9,21 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+headers=no
+while getopts h flag; do
+	case "${flag}" in
+	h) headers=yes ;;
+	esac
+done
+
 echo -e "${CYAN}Script started.${DEFAULT}"
-echo -e "${YELLOW}Installing modules and headers...${DEFAULT}"
+echo -e "${YELLOW}Installing modules...${DEFAULT}"
 cp -fr lib /usr
+echo -e "${GREEN}Installing modules finished.${DEFAULT}"
+if [ "$headers" = 'yes' ]; then
+echo -e "${YELLOW}Installing headers...${DEFAULT}"
 cp -fr include /usr
-echo -e "${GREEN}Installing modules and headers finished.${DEFAULT}"
+echo -e "${GREEN}Installing headers finished.${DEFAULT}"
+fi
 echo -e "${CYAN}Script finished. To install the kernel, set the path as shown here: https://learn.microsoft.com/en-us/windows/wsl/wsl-config#example-wslconfig-file${DEFAULT}"
 echo -e "${CYAN}After loading the new kernel, you can delete the modules for the previous one from /lib/modules.${DEFAULT}"
